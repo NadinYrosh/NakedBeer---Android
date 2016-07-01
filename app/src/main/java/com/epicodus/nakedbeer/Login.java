b30ea0a11fdee30320533d96e2e50ad3677bc752
Nadiya Yeroshkina <nadin.yeroshkina@gmail.com>
2016-07-01 11:53:35 -0700

    use butterknife and switch on loginActivity

--- app/src/main/java/com/epicodus/nakedbeer/Login.java
+++ app/src/main/java/com/epicodus/nakedbeer/Login.java
@@ -1,29 +1,34 @@
 package com.epicodus.nakedbeer;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
-public class Login extends AppCompatActivity {
+public class Login extends AppCompatActivity implements View.OnClickListener{
+    @Bind(R.id.bLogin)Button mbLogin;
+    @Bind(R.id.etUsername) EditText mUsername;
+    @Bind(R.id.etPassword) EditText mPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        ButterKnife.bind(this);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        mbLogin.setOnClickListener(this);
     }
 
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.bLogin:
+                break;
+        }
+    }
 }
