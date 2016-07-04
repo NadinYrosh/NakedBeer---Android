81cd3a39f7c3f34bfab5e958f31317037312fd81
Nadiya Yeroshkina <nadin.yeroshkina@email.com>
2016-07-04 11:45:46 -0700

    create input find field, grabing it with consoleLog

--- app/src/main/java/com/epicodus/nakedbeer/HomeActivity.java
+++ app/src/main/java/com/epicodus/nakedbeer/HomeActivity.java
@@ -2,12 +2,41 @@ package com.epicodus.nakedbeer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class HomeActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class HomeActivity extends AppCompatActivity implements View.OnClickListener{
+    @Bind(R.id.BeerInput) EditText mBeerInput;
+    @Bind(R.id.bFind)Button mFindBeer;
+
+    public static final String TAG = HomeActivity.class.getSimpleName();
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        ButterKnife.bind(this);
+
+        mFindBeer.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        String beer = mBeerInput.getText().toString();
+        Log.d(TAG, beer);
+        //switch (view.getId()){
+            //case R.id.bRegister:
+                //break;
+        //}
     }
 }
