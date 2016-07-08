685db786a9146471ada2a7aecf23a594954ead17
Nadiya Yeroshkina <nadin.yeroskina@gmail.com>
2016-07-08 09:42:54 -0700

    add api key and set up constants file with gradle properties

---
+++ app/src/main/java/com/epicodus/nakedbeer/Constants.java
@@ -0,0 +1,8 @@
+package com.epicodus.nakedbeer;
+
+
+public class Constants {
+    public static final String Key = BuildConfig.Key;
+    public static final String Base_URL = "http://api.brewerydb.com/v2/";
+    public static final String Api_Key_Parameter = "key";
+}
