2fa8fd52f3a590bbabc15af7c6dfbc3ea92fcd58
Nadiya Yeroshkina <nadin.yeroshkina@gmail.com>
2016-07-11 17:41:35 -0700

    creating recyclerView, did not saved last time for some reason

---
+++ app/src/main/java/com/epicodus/nakedbeer/adapters/StyleListAdapter.java
@@ -0,0 +1,10 @@
+package com.epicodus.nakedbeer.adapters;
+
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+public class StyleListAdapter extends RecyclerView.Adapter<StyleListAdapter.>{
+
+
+}
