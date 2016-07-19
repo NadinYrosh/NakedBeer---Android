1ab4f3600ef76ca3f76ae80e024cc7ce450cd4bc
Nadiya Yeroshkina <nadin.yeroshkina@gmail.com>
2016-07-19 14:57:43 -0700

    add functionality for firebase account registration

--- app/src/main/java/com/epicodus/nakedbeer/ui/RegisterActivity.java
+++ app/src/main/java/com/epicodus/nakedbeer/ui/RegisterActivity.java
@@ -1,28 +1,45 @@
 package com.epicodus.nakedbeer.ui;
 
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.epicodus.nakedbeer.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.lang.reflect.InvocationTargetException;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
+    public static final String TAG = RegisterActivity.class.getSimpleName();
+
     @Bind(R.id.logoText) TextView mLogoText;
     @Bind(R.id.nameEditText) EditText mName;
     @Bind(R.id.emailEditText) EditText mEmail;
     @Bind(R.id.passwordEditText) EditText mPassword;
     @Bind(R.id.confirmPasswordEditText)EditText mPasswordConfirm;
     @Bind(R.id.createUserButton)Button mCreateUser;
+
+    private FirebaseAuth.AuthStateListener mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
 
     @Override
@@ -32,16 +49,68 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
         ButterKnife.bind(this);
 
        Typeface FFF_Tusj = Typeface.createFromAsset(getAssets(), "fonts/FFF_Tusj.ttf");
-        mLogoText.setTypeface(FFF_Tusj);
 
+        mLogoText.setTypeface(FFF_Tusj);
         mCreateUser.setOnClickListener(this);
+
+        mAuth = FirebaseAuth.getInstance();
+        createAuthStateListener();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
     }
 
     @Override
     public void onClick(View view) {
-        switch (view.getId()){
-            case R.id.createUserButton:
-                break;
+        if (view == mCreateUser) {
+            createNewUser();
         }
+    }
+
+    private void createNewUser() {
+        final String name = mName.getText().toString().trim();
+        final String email = mEmail.getText().toString().trim();
+        String password = mPassword.getText().toString().trim();
+        String confirmPassword = mPasswordConfirm.getText().toString().trim();
+
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Log.d(TAG, "Authentication successful");
+                } else {
+                    Toast.makeText(RegisterActivity.this, "Authentication failed.",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private void createAuthStateListener() {
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                final FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    Intent intent = new Intent(RegisterActivity.this, HomeActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+
+        };
     }
 }
