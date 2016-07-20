f292021f39014e1d9a0b4e8676510a816415f43b
Nadiya Yeroshkina <nadin.yeroshkina@gmail.com>
2016-07-19 19:06:23 -0700

    add authStateListener to logIn page

--- app/src/main/java/com/epicodus/nakedbeer/ui/LoginActivity.java
+++ app/src/main/java/com/epicodus/nakedbeer/ui/LoginActivity.java
@@ -2,14 +2,21 @@ package com.epicodus.nakedbeer.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.epicodus.nakedbeer.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 
 import butterknife.Bind;
@@ -22,6 +29,9 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     @Bind(R.id.bLogin)Button mLogin;
 
     private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    public static final String TAG = LoginActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,9 +44,61 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         mRegisterLink.setOnClickListener(this);
 
         mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
 
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+    private void loginWithPassword() {
+        String email = mEmail.getText().toString().trim();
+        String password = mPassword.getText().toString().trim();
+
+        if (email.equals("")) {
+            mEmail.setError("Please enter your email");
+            return;
+        }
+        if (password.equals("")) {
+            mPassword.setError("Password cannot be blank");
+            return;
+        }
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "signInWithEmail:onComplete:" + task.isSuccessful());
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "signInWithEmail", task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
     @Override
     public void onClick(View view) {
         if (view == mRegisterLink) {
@@ -47,19 +109,6 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             if (view == mLogin ) {
                 loginWithPassword();
             }
-        }
-    }
-
-    private void loginWithPassword() {
-        String email = mEmail.getText().toString().trim();
-        String password = mPassword.getText().toString().trim();
-        if (email.equals("")) {
-            mEmail.setError("Please enter your email");
-            return;
-        }
-        if (password.equals("")) {
-            mPassword.setError("Password cannot be blank");
-            return;
         }
     }
 }
