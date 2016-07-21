d7e7e91a53b21c3ffdc82247aa26307093b56185
Nadiya Yeroshkina <nadin.yeroshkina@gmail.com>
2016-07-20 18:11:08 -0700

    add icon

--- app/src/main/java/com/epicodus/nakedbeer/Constants.java
+++ app/src/main/java/com/epicodus/nakedbeer/Constants.java
@@ -4,8 +4,10 @@ package com.epicodus.nakedbeer;
 public class Constants {
     public static final String Key = BuildConfig.Key;
     public static final String Base_URL = "http://api.brewerydb.com/v2/search/";
-    public static final String Api_Key_Parameter = "key";
     public static final String FIREBASE_CHILD_SEARCH_BEER_STYLE = "searchBeerStyle";
     public static final String FIREBASE_CHILD_BEER_STYLES = "beerStyles";
     public static final String PREFERENCES_BEERSTYLE_KEY = "beerstyle";
+    public static final String KEY_SOURCE = "source"; //represent the key in the key-value pair of the intent extra.
+    public static final String SOURCE_SAVED = "saved"; //corresponding value when the user navigates to this fragment through the SavedRestaurantListActivity
+    public static final String SOURCE_FIND = "find"; //represent the value when the user travels from the BeerStyleListActivity
 }
