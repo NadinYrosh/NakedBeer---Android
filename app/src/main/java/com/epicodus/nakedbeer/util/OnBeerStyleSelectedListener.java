0e29458c5ca41821cc0942eb0d9257bc653c020d
Nadiya Yeroshkina <nadin.yeroshkina@gmail.com>
2016-07-22 16:02:12 -0700

    tryed to remove save btn and it didnt work. Cleaned code ans styles.

---
+++ app/src/main/java/com/epicodus/nakedbeer/util/OnBeerStyleSelectedListener.java
@@ -0,0 +1,10 @@
+package com.epicodus.nakedbeer.util;
+
+import com.epicodus.nakedbeer.models.BeerStyle;
+
+import java.util.ArrayList;
+
+
+public class OnBeerStyleSelectedListener {
+    public void onBeerStyleSelected(Integer position, ArrayList<BeerStyle> beerStyles);
+}
